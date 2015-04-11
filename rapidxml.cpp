007128f24e8fd46f0ce487e029a58cc0babf709d
Arzaroth Lekva <lekva@arzaroth.com>
2015-04-11 13:44:51 +0900

    such macro

--- rapidxml.cpp
+++ rapidxml.cpp
@@ -10,6 +10,11 @@
 #include <Python.h>
 #include <rapidxml.hpp>
 
+#define STR(x) #x
+#define STRINGIFY(x) STR(x)
+#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
+#define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
+
 struct module_state {
   PyObject *error;
 };
@@ -22,20 +27,26 @@ static struct module_state _state;
 #endif
 
 #define MODULE_DESC "rapidxml module for rapidxml bindings"
+#define MODULE_NAME rapidxml
+#define OBJ_NAME RapidXml
+
+#define OBJ_TYPE CAT(MODULE_NAME, CAT(_, CAT(OBJ_NAME, Type)))
+#define OBJ_OBJECT CAT(MODULE_NAME, CAT(_, CAT(OBJ_NAME, Object)))
+#define MODULE_METHS CAT(MODULE_NAME, _methods)
 
 typedef struct {
   PyObject_HEAD
-} rapidxml_RapidXmlObject;
+} OBJ_OBJECT;
 
-static PyTypeObject rapidxml_RapidXmlType = {
+static PyTypeObject OBJ_TYPE = {
   #if PY_MAJOR_VERSION >= 3
   PyVarObject_HEAD_INIT(NULL, 0)
   #else
   PyObject_HEAD_INIT(NULL)
   0,                               /*ob_size*/
   #endif
-  "rapidxml.RapidXml",             /*tp_name*/
-  sizeof(rapidxml_RapidXmlObject), /*tp_basicsize*/
+  STRINGIFY(MODULE_NAME) "." STRINGIFY(OBJ_NAME), /*tp_name*/
+  sizeof(OBJ_OBJECT), /*tp_basicsize*/
   0,                               /*tp_itemsize*/
   0,                               /*tp_dealloc*/
   0,                               /*tp_print*/
@@ -56,7 +67,7 @@ static PyTypeObject rapidxml_RapidXmlType = {
   "RapidXml objects",              /* tp_doc */
 };
 
-static PyMethodDef rapidxml_methods[] = {
+static PyMethodDef MODULE_METHS[] = {
   {NULL}
 };
 
@@ -74,10 +85,10 @@ static int module_clear(PyObject* m) {
 
 static struct PyModuleDef moduledef = {
   PyModuleDef_HEAD_INIT,
-  "rapidxml",
+  STRINGIFY(MODULE_NAME),
   MODULE_DESC,
   sizeof(struct module_state),
-  rapidxml_methods,
+  MODULE_METHS,
   NULL,
   module_traverse,
   module_clear,
@@ -86,37 +97,42 @@ static struct PyModuleDef moduledef = {
 
 # define INITERROR return NULL
 
-PyMODINIT_FUNC PyInit_rapidxml(void)
+PyMODINIT_FUNC CAT(PyInit_, MODULE_NAME)(void)
 
 #else
 # define INITERROR return
 
-PyMODINIT_FUNC initrapidxml(void)
+PyMODINIT_FUNC CAT(init, MODULE_NAME)(void)
 #endif
 {
   PyObject* module;
 
-  rapidxml_RapidXmlType.tp_new = PyType_GenericNew;
-  if (PyType_Ready(&rapidxml_RapidXmlType) < 0)
+  OBJ_TYPE.tp_new = PyType_GenericNew;
+  if (PyType_Ready(&OBJ_TYPE) < 0)
     INITERROR;
 
 #if PY_MAJOR_VERSION >= 3
   module = PyModule_Create(&moduledef);
 #else
-  module = Py_InitModule3("rapidxml", rapidxml_methods, MODULE_DESC);
+  module = Py_InitModule3(STRINGIFY(MODULE_NAME),
+                          MODULE_METHS,
+                          MODULE_DESC);
 #endif
   if (module == NULL)
     INITERROR;
 
   struct module_state* st = GETSTATE(module);
-  st->error = PyErr_NewException("rapidxml.Error", NULL, NULL);
+  st->error = PyErr_NewException(STRINGIFY(MODULE_NAME) ".Error",
+                                 NULL, NULL);
   if (st->error == NULL) {
     Py_DECREF(module);
     INITERROR;
   }
 
-  Py_INCREF(&rapidxml_RapidXmlType);
-  PyModule_AddObject(module, "RapidXml", (PyObject *)&rapidxml_RapidXmlType);
+  Py_INCREF(&OBJ_TYPE);
+  PyModule_AddObject(module,
+                     STRINGIFY(OBJ_NAME),
+                     (PyObject *)&OBJ_TYPE);
 
 #if PY_MAJOR_VERSION >= 3
   return module;
