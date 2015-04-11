dec1d62d1cb362785d4a2f50549f1115f2b6cd30
Arzaroth Lekva <lekva@arzaroth.com>
2015-04-11 16:21:36 +0900

    init & parse

--- rapidxml.cpp
+++ rapidxml.cpp
@@ -8,6 +8,7 @@
 */
 
 #include <Python.h>
+#include <structmember.h>
 #include <rapidxml.hpp>
 
 #define STR(x) #x
@@ -19,52 +20,113 @@ struct module_state {
   PyObject *error;
 };
 
-#if PY_MAJOR_VERSION >= 3
-# define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
-#else
-# define GETSTATE(m) (&_state)
-static struct module_state _state;
-#endif
-
 #define MODULE_DESC "rapidxml module for rapidxml bindings"
 #define MODULE_NAME rapidxml
-#define OBJ_NAME RapidXml
-
-#define OBJ_TYPE CAT(MODULE_NAME, CAT(_, CAT(OBJ_NAME, Type)))
-#define OBJ_OBJECT CAT(MODULE_NAME, CAT(_, CAT(OBJ_NAME, Object)))
 #define MODULE_METHS CAT(MODULE_NAME, _methods)
+
+#define OBJECT_NAME RapidXml
+#define OBJ_TYPE CAT(MODULE_NAME, CAT(_, CAT(OBJECT_NAME, Type)))
+#define OBJ_OBJECT CAT(MODULE_NAME, CAT(_, CAT(OBJECT_NAME, Object)))
+#define OBJ_METHS CAT(OBJECT_NAME, _methods)
+#define OBJ_MEMBERS CAT(OBJECT_NAME, _members)
+
+#define EXCEPT_NAME CAT(OBJECT_NAME, Error)
+
+static PyObject* EXCEPT_NAME;
 
 typedef struct {
   PyObject_HEAD
+  rapidxml::xml_document<> doc;
 } OBJ_OBJECT;
+
+static void CAT(OBJECT_NAME, _dealloc)(OBJ_OBJECT* self) {
+  #if PY_MAJOR_VERSION >= 3
+  Py_TYPE(self)->tp_free((PyObject*)self);
+  #else
+  self->ob_type->tp_free((PyObject*)self);
+  #endif
+}
+
+static PyObject* CAT(OBJECT_NAME, _new)(PyTypeObject* type,
+                                        PyObject* args,
+                                        PyObject* kwds) {
+  OBJ_OBJECT* self;
+
+  self = (OBJ_OBJECT*)type->tp_alloc(type, 0);
+  return (PyObject*)self;
+}
+
+static int CAT(OBJECT_NAME, _init)(OBJ_OBJECT* self,
+                                   PyObject* args,
+                                   PyObject* kwds) {
+  char* text;
+
+  static char* kwlist[] = {"text", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "s", kwlist,
+                                   &text)) {
+    return -1;
+  }
+  try {
+    self->doc.parse<0>(text);
+  } catch (rapidxml::parse_error &e) {
+    PyErr_SetString(EXCEPT_NAME, e.what());
+    return -1;
+  }
+  return 0;
+}
+
+static PyMemberDef OBJ_MEMBERS[] = {
+  {NULL}
+};
+
+static PyMethodDef OBJ_METHS[] = {
+  {NULL}
+};
 
 static PyTypeObject OBJ_TYPE = {
   #if PY_MAJOR_VERSION >= 3
   PyVarObject_HEAD_INIT(NULL, 0)
   #else
   PyObject_HEAD_INIT(NULL)
-  0,                               /*ob_size*/
+  0,                               /* ob_size */
   #endif
-  STRINGIFY(MODULE_NAME) "." STRINGIFY(OBJ_NAME), /*tp_name*/
-  sizeof(OBJ_OBJECT), /*tp_basicsize*/
-  0,                               /*tp_itemsize*/
-  0,                               /*tp_dealloc*/
-  0,                               /*tp_print*/
-  0,                               /*tp_getattr*/
-  0,                               /*tp_setattr*/
-  0,                               /*tp_reserved*/
-  0,                               /*tp_repr*/
-  0,                               /*tp_as_number*/
-  0,                               /*tp_as_sequence*/
-  0,                               /*tp_as_mapping*/
-  0,                               /*tp_hash */
-  0,                               /*tp_call*/
-  0,                               /*tp_str*/
-  0,                               /*tp_getattro*/
-  0,                               /*tp_setattro*/
-  0,                               /*tp_as_buffer*/
-  Py_TPFLAGS_DEFAULT,              /*tp_flags*/
+  STRINGIFY(MODULE_NAME) "." STRINGIFY(OBJECT_NAME), /* tp_name */
+  sizeof(OBJ_OBJECT),              /* tp_basicsize */
+  0,                               /* tp_itemsize */
+  (destructor)CAT(OBJECT_NAME, _dealloc), /* tp_dealloc */
+  0,                               /* tp_print */
+  0,                               /* tp_getattr */
+  0,                               /* tp_setattr */
+  0,                               /* tp_reserved */
+  0,                               /* tp_repr */
+  0,                               /* tp_as_number */
+  0,                               /* tp_as_sequence */
+  0,                               /* tp_as_mapping */
+  0,                               /* tp_hash  */
+  0,                               /* tp_call */
+  0,                               /* tp_str */
+  0,                               /* tp_getattro */
+  0,                               /* tp_setattro */
+  0,                               /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
   "RapidXml objects",              /* tp_doc */
+  0,                               /* tp_traverse */
+  0,                               /* tp_clear */
+  0,                               /* tp_richcompare */
+  0,                               /* tp_weaklistoffset */
+  0,                               /* tp_iter */
+  0,                               /* tp_iternext */
+  OBJ_METHS,                       /* tp_methods */
+  OBJ_MEMBERS,                     /* tp_members */
+  0,                               /* tp_getset */
+  0,                               /* tp_base */
+  0,                               /* tp_dict */
+  0,                               /* tp_descr_get */
+  0,                               /* tp_descr_set */
+  0,                               /* tp_dictoffset */
+  (initproc)CAT(OBJECT_NAME, _init), /* tp_init */
+  0,                               /* tp_alloc */
+  CAT(OBJECT_NAME, _new),          /* tp_new */
 };
 
 static PyMethodDef MODULE_METHS[] = {
@@ -73,25 +135,15 @@ static PyMethodDef MODULE_METHS[] = {
 
 #if PY_MAJOR_VERSION >= 3
 
-static int module_traverse(PyObject* m, visitproc visit, void* arg) {
-  Py_VISIT(GETSTATE(m)->error);
-  return 0;
-}
-
-static int module_clear(PyObject* m) {
-  Py_CLEAR(GETSTATE(m)->error);
-  return 0;
-}
-
 static struct PyModuleDef moduledef = {
   PyModuleDef_HEAD_INIT,
   STRINGIFY(MODULE_NAME),
   MODULE_DESC,
-  sizeof(struct module_state),
+  -1,
   MODULE_METHS,
   NULL,
-  module_traverse,
-  module_clear,
+  NULL,
+  NULL,
   NULL
 };
 
@@ -121,19 +173,17 @@ PyMODINIT_FUNC CAT(init, MODULE_NAME)(void)
   if (module == NULL)
     INITERROR;
 
-  struct module_state* st = GETSTATE(module);
-  st->error = PyErr_NewException(STRINGIFY(MODULE_NAME) ".Error",
-                                 NULL, NULL);
-  if (st->error == NULL) {
-    Py_DECREF(module);
-    INITERROR;
-  }
-
   Py_INCREF(&OBJ_TYPE);
   PyModule_AddObject(module,
-                     STRINGIFY(OBJ_NAME),
+                     STRINGIFY(OBJECT_NAME),
                      (PyObject *)&OBJ_TYPE);
 
+  EXCEPT_NAME = PyErr_NewException(STRINGIFY(MODULE_NAME) "." STRINGIFY(EXCEPT_NAME),
+                                   NULL, NULL);
+  Py_INCREF(EXCEPT_NAME);
+  PyModule_AddObject(module,
+                     STRINGIFY(EXCEPT_NAME),
+                     EXCEPT_NAME);
 #if PY_MAJOR_VERSION >= 3
   return module;
 #endif
