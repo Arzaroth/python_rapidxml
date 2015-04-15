4d36bec65d6c271ecd70d47686e1614f3c53cc34
Arzaroth Lekva <lekva@arzaroth.com>
2015-04-15 10:56:31 +0900

    typo, remove nodes

--- src/document_object.cpp
+++ src/document_object.cpp
@@ -182,7 +182,7 @@ PyTypeObject rapidxml_DocumentType = {
   0,                               /* tp_getattro */
   0,                               /* tp_setattro */
   0,                               /* tp_as_buffer */
-  Py_TPFLAGS_DEFAULT,              /*tp_flags*/
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
   "class representing a rapidxml::xml_document", /* tp_doc */
   0,                               /* tp_traverse */
   0,                               /* tp_clear */
