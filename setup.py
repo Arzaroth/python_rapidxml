29f954f097c3b62a5ddaeb84ecd2d5c7ec6fe0e3
Arzaroth Lekva <lekva@arzaroth.com>
2015-04-18 10:56:19 +0900

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 
 from setuptools import setup, Extension
 
-VERSION = ("1", "0", "0-alpha.3")
+VERSION = ("1", "0", "0")
 
 rapidxml = Extension("rapidxml._rapidxml",
                      define_macros=[('MAJOR_VERSION', VERSION[0]),
