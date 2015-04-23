0c521b1fd211406e0cc9904e0112753873f2f75b
Arzaroth Lekva <lekva@arzaroth.com>
2015-04-23 13:00:02 +0900

    more tests

---
+++ tests/test_basic.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File: simple.py
+# by Arzaroth Lekva
+# arzaroth@arzaroth.com
+#
+
+import os
+import rapidxml
+
+def test_init(init_rapidxml):
+    assert init_rapidxml.unparse() == ('<root><test attr1="one" attr2="two" attr3="three"/>'
+                                       '<test2><node id="1"/><node id="2"/><node id="3"/></test2>'
+                                       '<test>some text</test></root>')
+    assert init_rapidxml.unparse() == repr(init_rapidxml)
+    assert init_rapidxml.unparse(True) == str(init_rapidxml)
+
+def test_parse(init_rapidxml):
+    r = rapidxml.RapidXml()
+    try:
+        data = init_rapidxml.unparse().encode('utf-8')
+    except UnicodeDecodeError:
+        data = init_rapidxml.unparse()
+    r.parse(bytearray(data))
+    assert str(r) == str(init_rapidxml)
+
+def test_parse_from_file(init_rapidxml, tmpdir):
+    f = tmpdir.join("dump.xml")
+    f.write(init_rapidxml.unparse())
+    r = rapidxml.RapidXml(str(f), from_file=True)
+    assert str(r) == str(init_rapidxml)
+
+def test_equals(init_rapidxml):
+    assert init_rapidxml == init_rapidxml
+    root = init_rapidxml.first_node()
+    assert root == root
+    assert root == init_rapidxml.first_node()
+    assert root.first_node() != root.first_node("test2")
+    assert (root != root) == (not (root == root))
+
+def test_first_node(init_rapidxml):
+    root = init_rapidxml.first_node()
+    assert root.name == "root"
+    assert root.value == ""
+    assert root.unparse() == init_rapidxml.unparse()
+
+def test_nested_node(init_rapidxml):
+    test = init_rapidxml.first_node().first_node("test")
+    assert test.name == "test"
+    assert test.value == ""
+    test2 = init_rapidxml.first_node().first_node("test2")
+    assert test2.name == "test2"
+    assert test2.value == ""
+
+def test_assign(init_rapidxml):
+    root = init_rapidxml.first_node()
+    root.name = "new_root"
+    assert root.name == "new_root"
+    test = root.first_node()
+    test.name = "new_test"
+    test.first_attribute().name = "new_attr1"
+    test.first_attribute().next_attribute().value = "new_two"
+    test = root.first_node("test")
+    test.value = "some new text"
+    assert test.value == "some new text"
+    assert init_rapidxml.unparse() == ('<new_root><new_test new_attr1="one" attr2="new_two" attr3="three"/>'
+                                       '<test2><node id="1"/><node id="2"/><node id="3"/></test2>'
+                                       '<test>some new text</test></new_root>')
---
+++ tests/test_iterations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File: test_iterations.py
+# by Arzaroth Lekva
+# arzaroth@arzaroth.com
+#
+
+def test_children(init_rapidxml):
+    test2 = init_rapidxml.first_node().first_node("test2")
+    i_node = test2.first_node()
+    for node in test2.children:
+        assert i_node == node
+        assert node.name == "node"
+        assert node.value == ""
+        i_node = i_node.next_sibling()
+        if i_node:
+            assert i_node != node
+
+def test_attributes(init_rapidxml):
+    test = init_rapidxml.first_node().first_node()
+    i_attr = test.first_attribute()
+    for attr, expected_attr in zip(test.attributes, [('attr1', 'one'),
+                                                     ('attr2', 'two'),
+                                                     ('attr3', 'three')]):
+        assert i_attr == attr
+        assert (attr.name, attr.value) == expected_attr
+        assert (i_attr.name, i_attr.value) == expected_attr
+        i_attr = i_attr.next_attribute()
+        if i_attr:
+            assert i_attr != attr
